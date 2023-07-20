2e6d4fd03d7bd0ff2df61b7ff830140e9cb0f550
RenanMoraesCardoso <99659953+RenanMoraesCardoso@users.noreply.github.com>
2023-07-20 15:33:21 -0300

    prova econ 4.R
    Fechei a Q1

--- prova econ 4.R
+++ prova econ 4.R
@@ -4,22 +4,24 @@ library(stats)
 library(readxl)
 library(ggplot2)
 library(tidyverse)
+library(GGally)
 #a
 returns<-read_excel("returns.xlsx")
-pca<-returns[,-(1:17)]
-pca_pad<-scale(returns[,-(1:17)])
-pca_result <- prcomp(pca)
-pca_pad_result <- prcomp(pca_pad)
-plot(pca_result$x[,1], pca_result$x[,2], xlab = "PC1", ylab = "PC2")
+returns_pca<-returns[,-(1:17)]
+returns_pca_pad<-scale(returns[,-(1:17)])
+returns_pca_result <- prcomp(returns_pca)
+returns_pca_pad_result <- prcomp(returns_pca_pad)
+plot(returns_pca_result$x[,1], returns_pca_result$x[,2], xlab = "PC1", ylab = "PC2")
+
 
 #otimo
 #Informal Way
 
-prop_var <- (pca_result$sdev^2) / sum(pca_result$sdev^2)
+prop_var <- (returns_pca_result$sdev^2) / sum(returns_pca_result$sdev^2)
 prop_var_acum <- cumsum(prop_var)
 plot(prop_var_acum, type = "b", xlab = "Número de Componentes", ylab = "Variância Explicada Acumulada")
 
-prop_pad_var <- (pca_pad_result$sdev^2) / sum(pca_pad_result$sdev^2)
+prop_pad_var <- (returns_pca_pad_result$sdev^2) / sum(returns_pca_pad_result$sdev^2)
 prop_pad_var_acum <- cumsum(prop_pad_var)
 plot(prop_pad_var_acum, type = "b", xlab = "Número de Componentes", ylab = "Variância Explicada Acumulada")
 
@@ -42,15 +44,16 @@ for(i in 2:length(prop_var_acum)){
 }
 thumb_pad
 
-# Pela regra de bolso foram selecionados 2 componentes.
+# Pela regra de bolso de adicionar os componentes que incrementam a variância explicada
+# em pelo menos 3% foram selecionados 2 componentes.
 
 #########Biggest Drop
-autovalores<-pca_result$sdev
+autovalores<-returns_pca_result$sdev
 autovalores1<<-c(autovalores[-1],1)
 biggest_drop<-which.max(autovalores/autovalores1)
 biggest_drop
 
-autovalores_pad<-pca_pad_result$sdev
+autovalores_pad<-returns_pca_pad_result$sdev
 autovalores1_pad<<-c(autovalores_pad[-1],1)
 biggest_drop_pad<-which.max(autovalores_pad/autovalores1_pad)
 biggest_drop_pad
@@ -60,26 +63,21 @@ biggest_drop_pad
 
 # Pela regra de bolso foram selecionados os 2 primeiros componentes. Usando as variáveis padronizadas, os 2
 # primeiros componentes explicam:
-print(summary_pca$importance["Cumulative Proportion","PC2"])
+summary_returns_pca <- summary(returns_pca_pad_result)
+print(summary_returns_pca$importance["Cumulative Proportion","PC2"])
 ##### 0.09503 da variância.
 
 ## b)
 
-pc1<-pca_pad_result$x[,1]
-dados<-data.frame(pc1, scale(returns[,2:17]))
-scale(returns[,2:17])
-ggplot(dados, aes(pc1, MKT)) +
-  geom_point() +
-  labs(x = "Variável Independente", y = "Variável Dependente") +
-    ylim(-0.2, 0.15) +  # Definir limites no eixo x
-    xlim(-15, 25) 
-cor(pca_result$x[,1], returns[,2])
-pca1<-pca_result$x[,1]
-returns_reg<-(cbind(pca1,returns[2:17]))
-reg1<-lm( pca1~MKT + HML + SMB + MOM1 +  MOM36 + ACC + BETA + CFP+
-           DY + EP + IDIOVOL + CMA + UMD + RMW + RETVOL + CHCSHO, data= returns_reg)
+# Computando a relação do primeiro componente com as anomalias:
 
+pc1_pad<-returns_pca_pad_result$x[,1]
+pc1<-returns_pca_result$x[,1]
 
+#
+returns_reg<-(cbind(pc1_pad,returns[2:17]))
+reg1<-lm( pc1_pad~MKT + HML + SMB + MOM1 +  MOM36 + ACC + BETA + CFP+
+            DY + EP + IDIOVOL + CMA + UMD + RMW + RETVOL + CHCSHO, data= returns_reg)
 
 # Extrair os coeficientes e intervalos de confiança do modelo
 coeficientes <- coef(reg1)[-1]
@@ -101,8 +99,47 @@ ggplot(df_coeficientes, aes(x = Coeficiente, y = Valor)) +
   ggtitle("Boxplot dos Coeficientes da Regressão Múltipla") +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
+summary(reg1)
 
-###########
+# Computando a relação do segundo componente com as anomalias:
+
+pc2_pad<-returns_pca_pad_result$x[,2]
+pc2<-returns_pca_result$x[,2]
+
+returns_reg2<-(cbind(pc2_pad,returns[2:17]))
+reg2<-lm( pc2_pad~MKT + HML + SMB + MOM1 +  MOM36 + ACC + BETA + CFP+
+            DY + EP + IDIOVOL + CMA + UMD + RMW + RETVOL + CHCSHO, data= returns_reg2)
+
+# Extrair os coeficientes e intervalos de confiança do modelo
+coeficientes2 <- coef(reg2)[-1]
+intervalos_confianca2 <- confint(reg2)[-1,]
+
+# Converter os coeficientes e intervalos de confiança em um data frame
+df_coeficientes2 <- data.frame(Coeficiente2 = names(coeficiente2),
+                              Valor2 = coeficientes2,
+                              IC_Inferior2 = intervalos_confianca2[,1],
+                              IC_Superior2 = intervalos_confianca2[,2])
+
+# Criar o gráfico de boxplot com os coeficientes e intervalos de confiança
+ggplot(df_coeficientes2, aes(x = Coeficiente2, y = Valor2)) +
+  geom_boxplot() +
+  geom_errorbar(aes(ymin = IC_Inferior2, ymax = IC_Superior2),
+                width = 0.2, color = "red") +
+  xlab("Coeficiente") +
+  ylab("Valor") +
+  ggtitle("Boxplot dos Coeficientes da Regressão Múltipla") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+summary(reg2)
+
+## Como eles se relacionam? O R² e o R² ajustado da regressão com o PC1 foram 0.954 e 0.9516, e para a regressão
+# com o PC2 foram 0.6353 e 0.6164. Então os fatores anômalos contain quase toda a informação do PC1.
+# Os R²s são menores na regressão com o PC2, mas ainda consideravelmente altos.
+# A única anomalia que tem p-valor menor que 0.001 em ambas as regressões é o fator de mercado (MKT).
+# Isso pode ser interpretado pelo fato dos componentes principais serem combinações lineares dos retornos do mercado, 
+# logo faz sentido que o MKT faça um bom fit.
+
+## c) 
 
 
 pca16<-returns[,(2:17)]
@@ -141,13 +178,41 @@ for(i in 2:length(prop_var_acum)){
 }
 thumb_pad
 
-#########Biggest Drop
-autovalores<-pca16_result$sdev
-autovalores1<<-c(autovalores[-1],1)
-biggest_drop<-which.max(autovalores/autovalores1)
-biggest_drop
+# Pela regra de bolso de adicionar os componentes que incrementam a variância explicada
+# em pelo menos 3% foram selecionados 8 componentes considerando o PCA feito com as variáveis padronizadas.
 
-autovalores_pad<-pca16_pad_result$sdev
+# Inspecionando os autovetores em busca das variáveis com maior ponderação:
+
+# Olhando para o primeiro componente:
+relevant_vars_PC1 <- rownames(pca16_pad_result$rotation)[order(abs(pca16_pad_result$rotation[, 1]), decreasing = TRUE)]
+print(relevant_vars_PC1)
+  
+# Olhando para o segundo componente:
+relevant_vars_PC2 <- rownames(pca16_pad_result$rotation)[order(abs(pca16_pad_result$rotation[, 2]), decreasing = TRUE)]
+print(relevant_vars_PC2)
+
+# Olhando para o terceiro componente:
+relevant_vars_PC3 <- rownames(pca16_pad_result$rotation)[order(abs(pca16_pad_result$rotation[, 3]), decreasing = TRUE)]
+print(relevant_vars_PC3)
+
+# Olhando para o quarto componente:
+relevant_vars_PC4 <- rownames(pca16_pad_result$rotation)[order(abs(pca16_pad_result$rotation[, 4]), decreasing = TRUE)]
+print(relevant_vars_PC4)
+
+# Ao inspecionarmos os 4 primeiros componentes principais já constatamos que para cada um deles, as variáveis
+#mais relevantes (com maior peso) diferem, indicando que não há uma anomalia dominante para todos os componentes.
+# Por exemplo, para o 1º, a anomalia dominante é "IDIOVOL", para o 2º é "MOM36".
+
+## d)
+# Usando os componentes selecionados dos dois grupos:
+
+anomalies_returns_pcs <- data.frame(cbind(returns_pca_pad_result$x[,1:2], pca16_pad_result$x[,1:8]))
+colnames(anomalies_returns_pcs) <- c("Rtrns1", "Rtrns2", "Anmls1", "Anmls2", "Anmls3", "Anmls4", "Anmls5", "Anmls6", "Anmls7", "Anmls8")
+ggpairs(anomalies_returns_pcs)
+
+# A maior correlação aparece entre os dois primeiros componentes de ambos os grupos. Isso ocorre pois o PCA constrói os componentes
+# ordenando pela relevância explicativa das séries. Ao fazer o PCA de dois grupos de séries muito correlacionadas, é razoável
+# que a correlação maior aparece entre os vetores com mais informação sobre cada grupo, os primeiros vetores.
 
 #########
 ########## questao 2
